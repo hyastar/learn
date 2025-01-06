6a8004409c5b8979ccf829def92856773ace4a0d
hyastar <loli@mka.cc>
2025-01-06 20:27:34 +0800

    Implement user connection handling and data reception

--- cpp/projects/Network/testsocket/main.cpp
+++ cpp/projects/Network/testsocket/main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #ifdef _WIN32
 //#include <winsock2.h>
 #include <windows.h>
 //#include <ws2tcpip.h>
 //#pragma comment(lib, "ws2_32.lib")
+typedef int socklen_t;
 #else 
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -43,7 +45,25 @@ int main(int argc, char* argv[]) {
 	}
 	printf("bind port %d success!\n", port);
 	listen(sock, 10);
+	
+	sockaddr_in caddr;
+	socklen_t len = sizeof(caddr);
+	int client = accept(sock, (sockaddr*)&caddr, &len);
+	printf("accpet client %d\n", client);
+	char* ip = inet_ntoa(caddr.sin_addr);
+	unsigned short cport = ntohs(caddr.sin_port);
+	printf("client ip is %s, port is %d\n", ip, cport);
+	char buf[1024];
+	for (;;) {
+		int recvlen = recv(client, buf, sizeof(buf) - 1, 0);
+		if (recvlen <= 0) break;
+		buf[recvlen] = '\0';
+		if (strstr(buf, "quit") != NULL) break;
+		printf("recv %s\n", buf);
+	}
+	closesocket(client);
 
 	getchar();
+
 	return 0;
 }
